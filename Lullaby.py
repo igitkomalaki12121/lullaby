5771d7a23d1e32bde4d593a0269c6a70694157a9
SAGE-wsl <ladracristeve@gmail.com>
2025-09-08 07:50:37 +0800

    Add files via upload

---
+++ Lullaby.py
@@ -0,0 +1,31 @@
+import whois
+import dns.resolver
+
+def domain_info(target):
+    print(f"\[+] Gathering WHOIS info for: {target}")
+    try:
+        w = whois.whois(target)
+        print("Domain Name:", w.domain_name)
+        print("Registrar:", w.regristrar)
+        print("Creation Date:", w.creation_date)
+        print("Expiration Date:", w.expiration_date)
+        print("Name Servers:", w.name_servers)
+    except Exception as e:
+        print("WHOIS lookup failed:",e)
+
+    print(f"\n[+] Gathering DNS records fofr: {target}")
+    try:
+        for record in ["A", "AAA", "MX", "NS", "TXT"]:
+            try:
+                answers = dns.resolver.resolve(target, record)
+                print(f"{record} Records:")
+                for rdata in answers:
+                    print("   ", rdata.to_tex())
+            except Exception:
+                pass
+    except Exception as e:
+        print("DNS lookup failed:", e)
+
+if __name__ == "__main__":
+    target + input("Enter a domain (e.g., example.com)")
+    domain_info(target)
